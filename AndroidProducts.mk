5c2bc226323c290c25f8a9ab0c43a21d13baca8d
deadaf5 <abhinav.115260@gmail.com>
2023-02-27 09:10:37 +0530

    bringup_spark

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lancelot.mk
+    $(LOCAL_DIR)/spark_lancelot.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lancelot-user \
-    lineage_lancelot-userdebug \
-    lineage_lancelot-eng
+    spark_lancelot-user \
+    spark_lancelot-userdebug \
+    spark_lancelot-eng
